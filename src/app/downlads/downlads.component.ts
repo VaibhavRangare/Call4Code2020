26ec5333aa2020edeb111909924f8daa1de9161f
Vaibhav Rangare <47136076+VaibhavRangare@users.noreply.github.com>
2020-05-21 10:12:47 +0530

    Add files via upload

---
+++ src/app/downlads/downlads.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-downlads',
+  templateUrl: './downlads.component.html',
+  styleUrls: ['./downlads.component.scss']
+})
+export class DownladsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
